43e23f4bbc3d66bfd9cc3ef648347648670d464b
mr-bean-1 <renlau505@byui.edu>
2024-12-15 23:47:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+<h1>Videos</h1>
+            <p>These are the videos I use:</p>
+
+            <!-- Video Player -->
+            <video width="auto" height="300px" controls>
+                <source src="videos/can_you_feel_the_love_tonight.mp4" type="video/mp4">
+                <source src="videos/sonata_in_g.mp4" type="video/mp4">
+                <source src="videos/silent_night.mp4" type="video/mp4">
+                <source src="videos/deck_the_halls.mp4" type="video/mp4">
+
+                <!-- Fallback content -->
+                Your browser does not support the video tag.
+            </video>
+
+            <h1>Sheet Music</h1>
+            <p>Here is music that I use</p>
+        </main>
